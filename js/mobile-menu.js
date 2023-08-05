3b9dcb623b68be3d13d44419da89ff76adb6667e
anetten <anet.zvekova@gmail.com>
2023-08-05 21:49:25 +0300

    sum

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,14 @@
 (() => {
-  const reft = {
+  const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
   };
-  reft.openMenuBtn.addEventListener('click', toggleMenu);
-  reft.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    reft.menu.classList.toggle('is-open');
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
